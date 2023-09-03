47796889a10caea849576fc985168bdca7c00469
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-09-03 12:40:07 +0200

    Added NOT NULL constraints to database layout.

--- model/database_layout.sql
+++ model/database_layout.sql
@@ -1,76 +1,75 @@
+CREATE TABLE Suppliers(
+    SupplierId INTEGER PRIMARY KEY,
+    SuppliersName TEXT NOT NULL,
+    AddressId INTEGER NOT NULL
+);
+
 CREATE TABLE Products(
-    ProductId TEXT PRIMARY KEY Unique, 
+    ProductId INTEGER PRIMARY KEY, 
     ProductName TEXT NOT NULL,
-    Manufacturer TEXT,
+    Manufacturer TEXT NOT NULL,
     SupplierId INTEGER,
     Cost NUMERIC, -- what does it cost to buy this product from the supplier
     Price NUMERIC -- for how much is this product sold
 );
 
 CREATE TABLE Addresses(
-    AddressId INTEGER PRIMARY KEY Unique,
-    Street TEXT,
-    HouseNumber TEXT,
-    ZipCode TEXT,
-    CoordinateX REAL,
-    CoordinateY REAL
-);
-
-CREATE TABLE Suppliers(
-    SupplierId INTEGER PRIMARY KEY Unique,
-    SuppliersName TEXT,
-    AddressId INTEGER
+    AddressId INTEGER PRIMARY KEY,
+    Street TEXT NOT NULL,
+    HouseNumber TEXT NOT NULL,
+    ZipCode TEXT NOT NULL,
+    CoordinateX REAL NOT NULL,
+    CoordinateY REAL NOT NULL
 );
 
 CREATE TABLE Customers(
-    CustomerId INTEGER PRIMARY KEY Unique,
-    CustomerName TEXT,
-    AddressId INTEGER
+    CustomerId INTEGER PRIMARY KEY,
+    CustomerName TEXT NOT NULL,
+    AddressId INTEGER NOT NULL
 );
 
 CREATE TABLE Warehouses(
-    WarehouseId INTEGER PRIMARY KEY Unique,
-    WarehouseName TEXT,
-    AddressId INTEGER
+    WarehouseId INTEGER PRIMARY KEY,
+    WarehouseName TEXT NOT NULL,
+    AddressId INTEGER NOT NULL
 );
 
 CREATE TABLE Inventory(
-    WarehouseId INTEGER PRIMARY KEY Unique,
-    ProductId INTEGER,
-    Stock INTEGER, -- contraint to greater equal 0 
-    Capacity INTEGER -- contraint to greater equal 0 
+    WarehouseId INTEGER PRIMARY KEY,
+    ProductId INTEGER NOT NULL,
+    Stock INTEGER NOT NULL, -- contraint to greater equal 0 
+    Capacity INTEGER NOT NULL -- contraint to greater equal 0 
 );
 
 CREATE TABLE CustomerOrders(
-    OrderId INTEGER PRIMARY KEY Unique,
-    CustomerId INTEGER,
-    OrderDate TEXT, 
-    InvoiceId TEXT,
+    OrderId INTEGER PRIMARY KEY,
+    CustomerId INTEGER NOT NULL,
+    OrderDate TEXT NOT NULL, 
+    InvoiceId TEXT NOT NULL,
     FulfillmentDate TEXT,
-    Total NUMERIC -- since prices can change the total is not calculated from the Products table
+    Total NUMERIC NOT NULL -- since prices can change the total is not calculated from the Products table
 );
 
 CREATE TABLE CustomerOrderDetails(
-    OrderId INTEGER,
-    ProductId INTEGER,
-    Quantity INTEGER,
-    TotalPrice NUMERIC
+    OrderId INTEGER NOT NULL,
+    ProductId INTEGER NOT NULL,
+    Quantity INTEGER NOT NULL,
+    TotalPrice NUMERIC NOT NULL
 );
 
 CREATE TABLE SupplierOrders(
-    OrderId INTEGER,
-    SupplierId INTEGER,
-    OrderDate TEXT, 
-    InvoiceId TEXT,
+    OrderId INTEGER NOT NULL,
+    SupplierId INTEGER NOT NULL,
+    OrderDate TEXT NOT NULL, 
+    InvoiceId TEXT NOT NULL,
     FulfillmentDate TEXT,
-    Total NUMERIC -- since prices can change the total is not calculated from the Products table
+    Total NUMERIC NOT NULL -- since prices can change the total is not calculated from the Products table
 );
 
 CREATE TABLE SupplierOrderDetails(
-    OrderId INTEGER,
-    ProductId INTEGER,
-    Quantity NUMERIC,
-    Total NUMERIC
+    OrderId INTEGER NOT NULL,
+    ProductId INTEGER NOT NULL,
+    Quantity NUMERIC NOT NULL,
+    Total NUMERIC NOT NULL
 );
 
--- TODO foreign keys
