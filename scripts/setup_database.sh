07df36b189bf219386ec32a1d4a96481169cbed1
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-09-03 13:52:56 +0200

    Added listing of created tables to setup_databash.sh script.

--- scripts/setup_database.sh
+++ scripts/setup_database.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DATABASE_FILE_PATH="model/database.db"
+DATABASE_LAYOUT_SQL_FILE_PATH="model/database_layout.sql"
 
 # if the database file already exists delete it
 
@@ -10,4 +11,7 @@ if [[ -f ${DATABASE_FILE_PATH} ]]; then
 fi
 
 echo " * setup database layout in '${DATABASE_FILE_PATH}'"
-sqlite3 ${DATABASE_FILE_PATH} ".read model/database_layout.sql"
+sqlite3 ${DATABASE_FILE_PATH} ".read ${DATABASE_LAYOUT_SQL_FILE_PATH}"
+
+echo " * tables created in database:"
+echo "SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%'" | sqlite3 ${DATABASE_FILE_PATH}
