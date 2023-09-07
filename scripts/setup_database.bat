169f18adeb35f05ef36a78f5cbd86cab68ef6bf3
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-09-07 09:55:14 +0200

    Added echo to setup_database.bat when old database file is deleted.

--- scripts/setup_database.bat
+++ scripts/setup_database.bat
@@ -5,7 +5,10 @@ SET DATABASE_LAYOUT_SQL_FILE_PATH=".\\model\\database_layout.sql"
 SET EXAMPLE_DATA_SQL_FILE_PATH=".\\model\\example_data.sql"
 
 :: if the database file already exists delete it
-IF EXIST %DATABASE_FILE_PATH% DEL /F %DATABASE_FILE_PATH%
+IF EXIST %DATABASE_FILE_PATH% (
+	echo * deleting existing database file at '%DATABASE_FILE_PATH%'
+	DEL /F %DATABASE_FILE_PATH%
+)
 
 echo * setup database layout in '%DATABASE_FILE_PATH%
 duckdb %DATABASE_FILE_PATH% ".read %DATABASE_LAYOUT_SQL_FILE_PATH%"
@@ -15,3 +18,4 @@ duckdb %DATABASE_FILE_PATH% "SHOW TABLES;"
 
 echo * inserting example data into database
 duckdb %DATABASE_FILE_PATH% ".read %EXAMPLE_DATA_SQL_FILE_PATH%"
+
