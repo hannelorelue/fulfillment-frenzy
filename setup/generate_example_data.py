10685fa6dc4a1c4e1a8d471df8971884ce974de0
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-09-16 15:26:20 +0200

    Structured country and city retrieval.

--- setup/generate_example_data.py
+++ setup/generate_example_data.py
@@ -1,16 +1,22 @@
 import json
 import overpass
+import random
 
 from dataclasses import dataclass
 from iso3166 import countries
 from pprint import pprint
 from typing import List, Dict, Optional, Tuple
 
+random.seed(42)
+
 
 @dataclass
 class Country:
     name: str
     iso3166_1: str
+
+    def __hash__(self):
+        return hash(self.name)
 
 
 @dataclass
@@ -19,6 +25,23 @@ class City:
     name: str
     coordinates: Tuple[float, float]
     zipcode: Optional[str] = None
+
+    def __hash__(self):
+        return hash(self.node_id)
+
+
+@dataclass
+class Address:
+    street: str
+    house_number: str
+    city: str
+    zipcode: str
+    country: str
+    coordinate_latitude: float
+    coordinate_longitude: float
+
+    def __hash__(self):
+        return hash(self.street) ^ hash(self.house_number) ^ hash(self.city) ^ hash(self.zipcode)
 
 
 def get_countries() -> List[Country]:
@@ -59,10 +82,16 @@ node['place'='city'](area);
 out;
     """)
 
-    cities = [City(feature['id'], 
-                   feature['properties']['name'], 
-                   (feature['geometry']['coordinates'][1],feature['geometry']['coordinates'][0])
-                   ) for feature in response['features']]
+    cities = list()
+
+    for feature in response['features']:
+        try:
+            city = City(feature['id'], 
+                        feature['properties']['name'], 
+                        (feature['geometry']['coordinates'][1],feature['geometry']['coordinates'][0]))
+            cities.append(city)
+        except:
+            continue
 
     cities_with_zipcode = list()
 
@@ -114,8 +143,50 @@ def get_street_names_of_city(city: City, do_check: bool = True) -> Optional[List
     return list(street_names)
 
 
+def generate_addresses(countries: Optional[List[Country]] = None, number_of_countries: int = 20, number_of_cities: int = 100, number_of_addresses: int = 1000) -> List[Address]:
+    all_countries = get_countries()
+    
+    if countries is None:
+        selected_countries = random.sample(all_countries, number_of_countries)
+    else:
+        selected_countries = random.sample(all_countries, number_of_countries-len(countries))
+        selected_countries.extend(countries)
+
+
+    # sample number of cities from selected countries
+    min_number_of_cities_from_country = number_of_cities // number_of_countries // 3
+    max_number_of_cities_from_country = int(number_of_cities // number_of_countries * 1.1)
+
+    country_to_cities_map = dict()
+
+    sum_of_selected_cities = 0
+
+    # select cities from countries
+    for index, country in enumerate(selected_countries):
+        if index % 2 == 0:
+            number_of_cities_to_select_from_country = random.randint(min_number_of_cities_from_country, max_number_of_cities_from_country) 
+        else:
+            number_of_cities_to_select_from_country = (index+1) * (number_of_cities // number_of_countries) - sum_of_selected_cities
+
+        cities = get_cities_of_country(country)
+
+        # make sure not to select more cities than the country has
+        number_of_cities_to_select_from_country = min(number_of_cities_to_select_from_country, len(cities))
+
+        sum_of_selected_cities += number_of_cities_to_select_from_country
+
+        selected_cities = random.sample(cities, number_of_cities_to_select_from_country)
+        country_to_cities_map[country] = selected_cities 
+
+        print(f" * {index}: selected {len(country_to_cities_map[country])}/{len(cities)} cities for {country.name}")
+
+    for country, cities in country_to_cities_map.items():
+        # get streets of city
+        for city in cities:
+            ...            
+
 if __name__ == "__main__":
-    pprint(get_countries())
-    pprint(get_cities_of_country(Country("Germany", "DE")))
-    pprint(get_street_names_of_city(City(node_id=0, name="Rottenburg am Neckar", coordinates=(0,0)), do_check=False))
+    generate_addresses(countries=[Country("Germany", "DE"), 
+                                  Country("Croatia", "HR"), 
+                                  Country("United Kingdom", "UK")])
 
